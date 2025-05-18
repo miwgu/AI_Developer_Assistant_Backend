62e487bd2862ee6cdf4ea12a26931f5a5057a64a
Miwa Guhrés <miwa.guhres@hotmail.com>
2025-05-18 11:01:04 +0200

    Add cors setting

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/chat';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 
